9ee1611064b2b1a7e9aa1dd304b23fb3ceaa7859
ajita.sahay <ajita.sahay@infostretch.com>
2018-01-23 15:44:02 +0530

    appium

---
+++ src/test/java/com/shopclues/bean/ProductInfoBean.java
@@ -0,0 +1,28 @@
+package com.shopclues.bean;
+
+public class ProductInfoBean {
+
+	public static   String productName1;
+	public static String productPrice1;
+	
+	public static void setProductName(String productName)
+	{
+		productName1=productName;
+	}
+	
+	public static String getProductName()
+	{
+		return productName1;
+	}
+	
+	public static void setProductPrice(String productPrice)
+	{
+		productPrice1=productPrice;
+	}
+	
+	public static String getProductPrice()
+	{
+		return productPrice1;
+	}
+	
+}
